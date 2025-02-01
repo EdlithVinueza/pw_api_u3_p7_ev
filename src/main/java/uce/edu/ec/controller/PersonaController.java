de3cbcb41576dd0ba9b4982ddb2c1752346a8171
Edlith Vinueza <149545949+EdlithVinueza@users.noreply.github.com>
2025-01-31 19:43:31 -0500

    Refactor PersonaController to add CRUD operations using RESTful API

--- src/main/java/uce/edu/ec/controller/PersonaController.java
+++ src/main/java/uce/edu/ec/controller/PersonaController.java
@@ -1,9 +1,12 @@
 package uce.edu.ec.controller;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
 import uce.edu.ec.service.IPersonaService;
 import uce.edu.ec.service.to.PersonaTo;
 
@@ -16,29 +19,39 @@ public class PersonaController {
 
     @GET
     @Path("/buscar") // definimso nombre de la capacida
-    public Response buscarPorId() {
+    public PersonaTo buscarPorId() {
         Integer id = 1;
-        this.iPersonaService.buscarPorId(id);
-        return Response.ok(this.iPersonaService.buscarPorId(id)).build();
+        return this.iPersonaService.buscarPorId(id);
 
     }
 
-    @GET
+    @POST
     @Path("/guardar") // definimso nombre de la capacida
     public void guardar(PersonaTo personaTo) {
         this.iPersonaService.guardar(personaTo);
     }
 
-    @GET
+    @PUT
     @Path("/actualizar") // definimso nombre de la capacida
     public void actualizar(PersonaTo personaTo) {
         this.iPersonaService.actualizar(personaTo);
     }
 
-    @GET
+    @PATCH
+    @Path("/actualizar/parcial") // definimso nombre de la capacida
+    public void actualizarParcial(PersonaTo personaTo) { //debemos recibir un objeto to 
+        PersonaTo tmp = this.iPersonaService.buscarPorId(personaTo.getId());
+        if (personaTo.getNombre() != null) {
+            personaTo.setNombre(tmp.getNombre());
+        }
+        tmp.setNombre(personaTo.getNombre());
+        this.iPersonaService.actualizar(tmp);
+    }
+
+    @DELETE
     @Path("/borrar") // definimso nombre de la capacida
-    public void borrar(Integer id) {
-        this.iPersonaService.borrar(id);
+    public void borrar() {
+        this.iPersonaService.borrar(1);
     }
 
 }
